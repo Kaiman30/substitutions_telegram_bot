ffa437ae43cec6ed675ccf9bd482d574a9fadc23
Kaiman30 <kaiman4ik@internet.ru>
2023-10-14 17:40:24 +0300

    Пофикшен баг из-за которого замены не обновлялись вместе с сайтом

--- parsing.py
+++ parsing.py
@@ -1,14 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
 
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.text, "lxml")
-
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'
+}
 
 def parse_table():
+    
+    URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, "lxml")
+    
+    
     table = soup.find('div', class_="itemFullText").find('table').find_all('td')
     
     substitutions = []
@@ -28,23 +32,39 @@ def parse_table():
 
 
 def parse_day():
+    URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, "lxml")
+    
     day = soup.find('div', class_="itemFullText").find('p').text
     
     return day
 
 
 def parse_practice() -> str:
+    URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, "lxml")
+    
     practice = soup.find('div', class_="itemFullText").find_all('p')[1].text
     
     return practice
 
 
 def parse_duty():
+    URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, "lxml")
+    
     duty = soup.find('div', class_="itemFullText").find_all('p')[2].text
     
     return duty
 
 def parse_modifyDate():
+    URL = "https://auto-meh.ru/studentu/ochnoe-otdelenie/zameny-ochnogo-otdeleniya.html"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, "lxml")
+    
     modifyDate = soup.find('span', class_="itemDateModified").text
     
     return modifyDate
